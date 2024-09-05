ba4dd76a2f3cfac83fd033bb0bd00fa29278720a
Interceptor <darbaz.me@gmail.com>
2024-09-05 11:39:19 +0300

    Add assertion for subtract function in assertion module

--- src/modules/fundamentals/assertion.js
+++ src/modules/fundamentals/assertion.js
@@ -1,7 +1,14 @@
 import { sum, subtract } from "./math"
 
-const result = sum(3, 7)
-const expected = 10
+let result = sum(3, 7)
+let expected = 10
+
+if (result !== expected) {
+  throw new Error(`${result} is not equal to ${expected}`)
+}
+
+result = subtract(4, 4)
+expected = 0
 
 if (result !== expected) {
   throw new Error(`${result} is not equal to ${expected}`)
