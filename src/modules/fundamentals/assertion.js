941b1eca8bd5321fcc33ec9459ad8ee66d434f6c
Interceptor <darbaz.me@gmail.com>
2024-09-05 14:10:43 +0300

    Add test framework and tests for sum and subtract functions in assertion module

--- src/modules/fundamentals/assertion.js
+++ src/modules/fundamentals/assertion.js
@@ -1,15 +1,29 @@
-import { sum } from "./math.js"
+import { sum , subtract} from "./math.js"
 
 const expect = actual => {
   return {
     toBe(expected) {
       if (actual !== expected) {
-        console.log('actual: ', actual)
-        console.log('expected: ', expected)
         throw new Error(`${actual} is not equal to ${expected}`)
       }
     }
   }
 }
 
-expect(sum(3, 7)).toBe(10)
+const test = (title, callback) => {
+  try {
+    callback()
+    console.log(`${title}`)
+  } catch (error) {
+    console.error(title)
+    console.error(error)
+  }
+}
+
+test('sum', () => {
+  expect(sum(3, 7)).toBe(10)
+})
+
+test('subtract', () => {
+  expect(subtract(7, 7)).toBe(0)
+})
