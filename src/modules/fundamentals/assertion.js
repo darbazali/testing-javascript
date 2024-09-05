91eb24bacd65450edf54b22a245e82ba48332d7d
Interceptor <darbaz.me@gmail.com>
2024-09-05 11:43:38 +0300

    Refactor assertion logic to use `expect` function for improved readability and reusability

--- src/modules/fundamentals/assertion.js
+++ src/modules/fundamentals/assertion.js
@@ -1,15 +1,21 @@
 import { sum, subtract } from "./math"
 
+const expect = (actual) => {
+  return {
+    toBe(expected) {
+      if (actual !== expected) {
+        throw new Error(`${actual} is not equal to ${expected}`)
+      }
+    },
+  }
+}
+
 let result = sum(3, 7)
 let expected = 10
 
-if (result !== expected) {
-  throw new Error(`${result} is not equal to ${expected}`)
-}
+expect(result).toBe(expected)
 
 result = subtract(4, 4)
 expected = 0
 
-if (result !== expected) {
-  throw new Error(`${result} is not equal to ${expected}`)
-}
+expect(result).toBe(expected)
