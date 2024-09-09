af12cd613d14bc62748c0fd5142160699aca8808
Interceptor <darbaz.me@gmail.com>
2024-09-09 16:16:59 +0300

    Add asyncSum and asyncSubtract functions for asynchronous operations in math module

--- src/modules/fundamentals/math.js
+++ src/modules/fundamentals/math.js
@@ -1,4 +1,10 @@
 const sum = (a, b) => a + b
 const subtract = (a, b) => a - b
 
-export { sum, subtract }
+const asyncSum = (a, b) =>
+  new Promise((resolve) => setTimeout(() => resolve(a + b), 1000))
+
+const asyncSubtract = (a, b) =>
+  new Promise((resolve) => setTimeout(() => resolve(a - b), 1000))
+
+export { sum, subtract, asyncSum, asyncSubtract }
