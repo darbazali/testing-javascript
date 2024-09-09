dcdb8b7653f71fb2e314cee4c17289230a0696ee
Interceptor <darbaz.me@gmail.com>
2024-09-09 16:17:04 +0300

    Add tests for asyncSum and asyncSubtract functions in math module

--- src/modules/fundamentals/math.test.js
+++ src/modules/fundamentals/math.test.js
@@ -1,6 +1,6 @@
 import test from "./test-lib.js"
 import expect from "./assertion-lib.js"
-import { sum, subtract } from "./math.js"
+import { sum, subtract, asyncSum, asyncSubtract } from "./math.js"
 
 test("sum", () => {
   expect(sum(1, 2)).toBe(3)
@@ -8,4 +8,12 @@ test("sum", () => {
 
 test("subtract", () => {
   expect(subtract(1, 2)).toBe(-1)
+})
+
+test("asyncSum", async () => {
+  expect(await asyncSum(1, 2)).toBe(3)
+})
+
+test("asyncSubtract", async () => {
+  expect(await asyncSubtract(1, 2)).toBe(-1)
 })
